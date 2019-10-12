b3f1c0b705e4fca2bf2ba95a875250aa36f53704
Kim Sangyeon <dho113@naver.com>
2019-10-12 11:12:59 +0900

    react clock example

--- react_clock_example/src/App.js
+++ react_clock_example/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// css
+import "./App.css";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillMount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Clock Example</h1>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
